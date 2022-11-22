310d90892b3e84dd6def2f703b60b2b6809ad820
Efunnuga Bamidele <bamideleefunnuga9@gmail.com>
2022-11-22 13:01:51 +0100

    feat: Implement Dark Mode Class

--- utility-first/tailwind.config.js
+++ utility-first/tailwind.config.js
@@ -1,6 +1,17 @@
 module.exports = {
+  darkMode: 'class',
   content: ['./*.html'],
   theme: {
+    screens: {
+      sm: "550px",
+      md: "800px",
+      lg: "1200px",
+      xl: "1440px"
+    }, 
+    fontFamily:{
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
     extend: {},
   },
   plugins: [],
